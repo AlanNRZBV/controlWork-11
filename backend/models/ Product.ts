c83a74abef018a8c3d84d7b5edf19606f89a1a89
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2024-03-02 13:00:37 +0600

    backend.feat: added Product model

---
+++ backend/models/ Product.ts
@@ -0,0 +1,50 @@
+import mongoose, { Types } from 'mongoose';
+import User from './User';
+import Category from './Category';
+
+const Schema = mongoose.Schema
+
+const ProductSchema= new Schema ({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    validate: {
+      validator: async (value: Types.ObjectId) => {
+        const user = await User.findById(value);
+        return Boolean(user);
+      },
+      message: 'User does not exist!',
+    },
+  },
+  categoryId:{
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    validate: {
+      validator: async (value: Types.ObjectId) => {
+        const category = await Category.findById(value);
+        return Boolean(category);
+      },
+      message: 'Category does not exist!',
+    },
+  },
+  title:{
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price:{
+    type: Number,
+    required: true
+  },
+  image:{
+    type: String,
+    required:true
+  },
+})
+
+const Product = mongoose.model('Product', ProductSchema);
+
+export default Product;
